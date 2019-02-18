2488adff6b4d7c7ee858213c98c5ad8c7366792d
DecaIi <aigle1997@hotmail.fr>
2019-02-18 14:01:41 +0100

    Add pakage perso Add classe Perso

--- src/perso/Perso.java
+++ src/perso/Perso.java
@@ -6,9 +6,16 @@ import tools.Hitbox;
 public class Perso {
     private Coordinate position ;
     private Hitbox hitbox ;
+    private double Height ;
+    private double Width ;
 
     public Perso(Coordinate position, Hitbox hitbox) {
         this.position = position;
-        this.hitbox = hitbox;
+        this.hitbox = new Hitbox(position , Height , Width);
     }
+    public void Attaque() {
+        Hitbox att = new Hitbox( new Coordinate(position.getX() + Height / 2 , position.getY() + Width + 20 ) , 10 ,10 ) ;  // cree une hitbox  devan le perso
+    }
+
+
 }
