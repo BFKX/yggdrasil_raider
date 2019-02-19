04ef9fedbab79e75720d752537ff080ad88d703c
Kirish25 <Kirishaan@hotmail.Fr>
2019-02-19 14:21:27 +0100

    Rename Perso to character

--- src/character/Character.java
+++ src/character/Character.java
@@ -1,20 +1,20 @@
-package perso;
+package character;
 
 import tools.Coordinate;
 import tools.Hitbox;
 
-public class Perso {
+public class Character {
     private Coordinate position ;
     private Hitbox hitbox ;
     private double Height ;
     private double Width ;
 
-    public Perso(Coordinate position, Hitbox hitbox) {
+    public Character(Coordinate position, Hitbox hitbox) {
         this.position = position;
         this.hitbox = new Hitbox(position , Height , Width);
     }
     public void Attaque() {
-        Hitbox att = new Hitbox( new Coordinate(position.getX() + Height / 2 , position.getY() + Width + 20 ) , 10 ,10 ) ;  // cree une hitbox  devan le perso
+        Hitbox att = new Hitbox( new Coordinate(position.getX() + Height / 2 , position.getY() + Width + 20 ) , 10 ,10 ) ;  // cree une hitbox  devan le character
         System.out.println();
     }
 
