e9df7727d8e32df94783d0aa098f3f12de0ccf29
Kirish25 <Kirishaan@hotmail.Fr>
2019-02-25 12:41:50 +0100

    debut de déplacement

--- src/character/Character.java
+++ src/character/Character.java
@@ -2,21 +2,35 @@ package character;
 
 import tools.Coordinate;
 import tools.Hitbox;
+import javafx.scene.input.KeyCode;
+
 
 public class Character {
     private Coordinate position ;
     private Hitbox hitbox ;
-    private double Height ;
-    private double Width ;
+    private double height ;
+    private double width ;
 
     public Character(Coordinate position, Hitbox hitbox) {
         this.position = position;
-        this.hitbox = new Hitbox(position , Height , Width);
+        this.hitbox = new Hitbox(position , height , width);
     }
-    public void Attaque() {
-        Hitbox att = new Hitbox( new Coordinate(position.getX() + Height / 2 , position.getY() + Width + 20 ) , 10 ,10 ) ;  // cree une hitbox  devan le character
+    public void attaque() {
+        Hitbox att = new Hitbox( new Coordinate(position.getX() + height / 2 , position.getY() + width + 20 ) , 10 ,10 ) ;  // cree une hitbox  devan le character
         System.out.println();
     }
+    public void validatePosition(){
+    }
 
+    public void displacement(KeyCode code){
+        switch(code) {
+            case LEFT:  position.setX(position.getX() - 5); break;
+            case RIGHT: position.setX(position.getX() + 5); break;
+            case UP:    position.setY(position.getY() + 5); break;
+            case DOWN:  position.setY(position.getY() - 5); break;
+            default: break;
 
+        }
+
+    }
 }
