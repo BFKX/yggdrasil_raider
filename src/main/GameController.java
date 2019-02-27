cb42ac7c7d6992a1e5b078830e36be64faf18fb0
Bertaga97 <bertaga97@gmail.com>
2019-02-27 19:07:34 +0100

    Create a map that fits the screen and display it

--- src/main/GameController.java
+++ src/main/GameController.java
@@ -13,9 +13,14 @@ import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import mapping.Map;
 import tools.FPSMeter;
 
 import java.awt.Toolkit;
@@ -31,60 +36,65 @@ class GameController extends Application {
     private boolean pauseShown = false;
     private Button resumeButton = new Button("Resume");
     private Button quitButton = new Button("Quit");
+    private Text text = new Text("Pause");
 
-    @FXML private Text text;
     @FXML private AnchorPane game;
 
     GameController(Stage primaryStage) {
-
         this.primaryStage = primaryStage;
-
         this.pauseBackground = new Image("resources/images/menuBackground.png", WIDTH, HEIGHT, false, true);
     }
 
     @FXML void initialize() throws Exception {
-
         game.setPrefSize(WIDTH, HEIGHT);
+
+        Paint blue = Color.MEDIUMBLUE;
+
+        Font titleFont = Font.loadFont(StartMenuController.class.getResource("../resources/fonts/VIKING-N.TTF").toExternalForm(),
+                HEIGHT / 12);
+
+        text.setFont(titleFont);
+        text.setFill(Color.DARKORANGE);
+        text.setStroke(Color.DARKRED);
+        text.setStrokeWidth(HEIGHT / 360);
+        text.setLayoutX(WIDTH / 2 - text.getLayoutBounds().getWidth() / 2);
+        text.setLayoutY(HEIGHT / 2 + text.getLayoutBounds().getHeight() / 2);
 
         resumeButton.setPrefSize(WIDTH / 3, HEIGHT / 9);
         resumeButton.setLayoutX((WIDTH - resumeButton.getPrefWidth()) / 2);
         resumeButton.setLayoutY(HEIGHT * 0.25- resumeButton.getPrefHeight() / 2);
+        resumeButton.setBackground(new Background(new BackgroundFill(blue, null, null)));
         resumeButton.setOnAction(new EventHandler<ActionEvent>() {
-
             @Override
             public void handle(ActionEvent e) {
-
                 pause = false;
                 pauseShown = false;
                 game.getChildren().remove(quitButton);
                 game.getChildren().remove(resumeButton);
+                game.getChildren().remove(text);
             }
         });
 
         quitButton.setPrefSize(WIDTH / 3, HEIGHT / 9);
         quitButton.setLayoutX((WIDTH - resumeButton.getPrefWidth()) / 2);
         quitButton.setLayoutY(HEIGHT * 0.75- resumeButton.getPrefHeight() / 2);
+        quitButton.setBackground(new Background(new BackgroundFill(blue, null, null)));
         quitButton.setOnAction(new EventHandler<ActionEvent>() {
-
             @Override
             public void handle(ActionEvent e) {
-
                 System.exit(0);
             }
         });
 
         scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
-
             public void handle(KeyEvent e) {
-
                 if(e.getCode() == KeyCode.ESCAPE) {
-
                     pause = !pause;
                     if(!pause) {
-
                         pauseShown = false;
                         game.getChildren().remove(quitButton);
                         game.getChildren().remove(resumeButton);
+                        game.getChildren().remove(text);
                     }
                 }
             }
@@ -93,34 +103,38 @@ class GameController extends Application {
     }
 
     void setScene(Scene scene) {
-
         this.scene = scene;
     }
 
     @Override
     public void start(Stage stage) throws Exception {
-
         Canvas canvas = new Canvas(WIDTH, HEIGHT);
         game.getChildren().add(canvas);
         GraphicsContext gc = canvas.getGraphicsContext2D();
 
+        Map map = new Map(16, 9);
+        map.addGround();
+
         new AnimationTimer() {
+            long lastNow = 0;
 
             FPSMeter fpsm = new FPSMeter();
 
             public void handle(long now) {
-
-                if(!pause) {
-
+                if(!pause && now - lastNow >= 15000000) {
                     gc.setFill(Color.BLACK);
                     gc.fillRect(0, 0, WIDTH, HEIGHT);
 
-                    fpsm.update(now, gc);
-                } else if(!pauseShown){
+                    map.display(gc);
 
+                    fpsm.update(now, gc);
+
+                    lastNow = now;
+                } else if(!pauseShown && now - lastNow >= 15000000){
                     gc.drawImage(pauseBackground, 0, 0);
                     game.getChildren().add(quitButton);
                     game.getChildren().add(resumeButton);
+                    game.getChildren().add(text);
                     pauseShown = true;
                 }
             }
---
+++ src/mapping/Map.java
@@ -0,0 +1,57 @@
+package mapping;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import tools.Coordinate;
+import tools.Hitbox;
+
+import java.awt.Toolkit;
+
+public class Map {
+    private int[][] map;
+    private Case[][] mapCases;
+    private int lines;
+    private int columns;
+    private final double WIDTH = Toolkit.getDefaultToolkit().getScreenSize().getWidth();
+    private final double HEIGHT = Toolkit.getDefaultToolkit().getScreenSize().getHeight();
+    private Image ground = new Image("resources/images/ground.jpg");
+
+    public Map(int columns, int lines) {
+        this.lines = lines;
+        this.columns = columns;
+        this.map = new int[columns][lines];
+        this.mapCases = new Case[columns][lines];
+
+        for (int i = 0; i < columns; i++ ) {
+            for (int j = 0; j < lines; j++) {
+                this.mapCases[i][j] = new Case(0, null, null);
+            }
+        }
+    }
+
+    private void addCase(int column, int line, int type, Image image) {
+        this.map[column][line] = type ;
+        this.mapCases[column][line].setType(type);
+        this.mapCases[column][line].setImage(image);
+        this.mapCases[column][line].setHitbox(new Hitbox(new Coordinate(WIDTH * column / columns,HEIGHT * line / lines),WIDTH / columns,HEIGHT / lines));
+    }
+
+    public void addGround() {
+        for (int column = 0; column < columns; column++ ) {
+            addCase(column,lines - 1, 1, new Image("resources/images/ground.jpg"));
+        }
+    }
+
+    public void display(GraphicsContext gc) {
+        for (int column = 0; column < columns; column++ ) {
+            for (int line = 0; line < lines; line++) {
+                if(map[column][line] == 0) {
+                    gc.setFill(Color.BLACK);
+                } else if(map[column][line] == 1){
+                    gc.drawImage(ground, WIDTH * column / columns,HEIGHT * line / lines,WIDTH / columns,HEIGHT / lines);
+                }
+            }
+        }
+    }
+}
