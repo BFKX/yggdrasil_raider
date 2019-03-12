bc91d27fa47dea6fdf1f55761aecbcaecd528149
Kirish25 <Kirishaan@hotmail.Fr>
2019-03-12 10:41:06 +0100

    Petite correction

--- src/character/Character.java
+++ src/character/Character.java
@@ -11,29 +11,31 @@ public class Character {
 
     private Coordinate position;
     private Hitbox hitbox;
-    private final double height = Toolkit.getDefaultToolkit().getScreenSize().getHeight() / 40;
-    private final double width = Toolkit.getDefaultToolkit().getScreenSize().getHeight() / 40;
+    private final double HEIGHT = Toolkit.getDefaultToolkit().getScreenSize().getHeight() / 40;
+    private final double WIDTH = Toolkit.getDefaultToolkit().getScreenSize().getWidth() / 40;
 
     public Character(Coordinate position, Hitbox hitbox) {
 
         this.position = position;
-        this.hitbox = new Hitbox(position, width, height);
+        this.hitbox = new Hitbox(position, WIDTH, HEIGHT);
+        
     }
 
     public void attack() {
 
-        Hitbox att = new Hitbox(new Coordinate(position.getX() + width / 2, position.getY() + height + 20 ), 10, 10 );
+        Hitbox att = new Hitbox(new Coordinate(position.getX() + WIDTH / 2, position.getY() + HEIGHT + 20 ), 10, 10 );
     }
 
     public void validatePosition(){
+        }
     }
 
     public void displacement(KeyCode code){
         switch(code) {
-            case LEFT:  position.setX(position.getX() - 5); break;
-            case RIGHT: position.setX(position.getX() + 5); break;
-            case UP:    position.setY(position.getY() + 5); break;
-            case DOWN:  position.setY(position.getY() - 5); break;
+            case LEFT:  position.setX(position.getX() - 40); break;
+            case RIGHT: position.setX(position.getX() + 40); break;
+            case UP:    position.setY(position.getY() + 40); break;
+            case DOWN:  position.setY(position.getY() - 40); break;
             default: break;
 
         }
