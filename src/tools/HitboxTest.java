a6540e9ad32aa87362ede385824867cd7b21ff23
Bertaga97 <bertaga97@gmail.com>
2019-05-12 22:55:20 +0200

    Increase tests amount in tools.HitboxTest

--- src/tools/HitboxTest.java
+++ src/tools/HitboxTest.java
@@ -2,6 +2,8 @@ package tools;
 
 import org.junit.*;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -10,41 +12,39 @@ public class HitboxTest {
 
     @Test
     public void collideTest() {
-
-        // Equal Hitboxes
         Hitbox hitbox0 = new Hitbox(new Coordinate(0, 0), 1);
-        Hitbox hitbox1 = new Hitbox(new Coordinate(0, 0), 1);
-        assertTrue(hitbox0.collide(hitbox1));
+        Hitbox hitbox1;
 
-        // X movement
-        hitbox1 = new Hitbox(new Coordinate(1, 0), 1);
-        assertTrue(hitbox0.collide(hitbox1));
+        for (int i = 0; i < 1000000; i++) {
+            double z = randomSign() * ThreadLocalRandom.current().nextDouble(2);;
+            hitbox1 = new Hitbox(new Coordinate(z, 0), 1); // X movement
+            assertTrue(hitbox0.collide(hitbox1));
+            hitbox1 = new Hitbox(new Coordinate(0, z), 1); // Y movement
+            assertTrue(hitbox0.collide(hitbox1));
 
-        hitbox1 = new Hitbox(new Coordinate(2 * 999/1000, 0), 1);
-        assertTrue(hitbox0.collide(hitbox1));
+            double x = randomSign() * ThreadLocalRandom.current().nextDouble(Math.sqrt(2));
+            double y = randomSign() * ThreadLocalRandom.current().nextDouble(Math.sqrt(2));
+            hitbox1 = new Hitbox(new Coordinate(x, y), 1); // X + Y movement
+            assertTrue(hitbox0.collide(hitbox1));
+        }
 
-        hitbox1 = new Hitbox(new Coordinate(2, 0), 1);
+        hitbox1 = new Hitbox(new Coordinate(2, 0), 1); // X movement
         assertFalse(hitbox0.collide(hitbox1));
 
-        // Y movement
-        hitbox1 = new Hitbox(new Coordinate(0, 1), 1);
+        hitbox1 = new Hitbox(new Coordinate(2, 0), 2); // Modified size
         assertTrue(hitbox0.collide(hitbox1));
 
-        hitbox1 = new Hitbox(new Coordinate(0, 2), 1);
+        hitbox1 = new Hitbox(new Coordinate(0, 2), 1); // Y movement
         assertFalse(hitbox0.collide(hitbox1));
 
-        // X + Y movement
-        hitbox1 = new Hitbox(new Coordinate(1, 1), 1);
-        assertTrue(hitbox0.collide(hitbox1));
-
-        hitbox1 = new Hitbox(new Coordinate(2, 2), 1);
+        hitbox1 = new Hitbox(new Coordinate(Math.sqrt(2), Math.sqrt(2)), 1); // X + Y movement
         assertFalse(hitbox0.collide(hitbox1));
+    }
 
-        // Different sizes
-        hitbox1 = new Hitbox(new Coordinate(2, 0), 2);
-        assertTrue(hitbox0.collide(hitbox1));
-
-        hitbox1 = new Hitbox(new Coordinate(3, 0), 2);
-        assertFalse(hitbox0.collide(hitbox1));
+    private int randomSign() {
+        if (ThreadLocalRandom.current().nextDouble(2) < 1) {
+            return -1;
+        }
+        return 1;
     }
 }
