0cb0d1d4d5361b2567db9eb8097387ea1077e489
Bertaga97 <Bertaga97@gmail.com>
2019-05-25 16:54:31 +0200

    Reduce health loss

--- src/characters/Monster.java
+++ src/characters/Monster.java
@@ -5,37 +5,37 @@ import tools.Coordinate;
 import tools.Hitbox;
 
 public abstract class Monster extends Character {
-    Coordinate mainCharactersPosition ;
+    Coordinate mainCharactersPosition;
 
-    Monster(Coordinate position,Coordinate mainCharactersPosition){
+    Monster(Coordinate position, Coordinate mainCharactersPosition){
         super(position);
-        this.mainCharactersPosition =  mainCharactersPosition;
+        this.mainCharactersPosition = mainCharactersPosition;
     }
 
     public abstract void updateDisplacement();
 
     public abstract void display(GraphicsContext gc, Coordinate mainCharactersPosition);
 
-    void update(GraphicsContext gc) {
+    void update() {
         updateDisplacement();
-        this.position.add( speedX/SIDE, speedY/SIDE);
-    }
-    public void setMainCharactersPosition(Coordinate position){
-        this.mainCharactersPosition=position;
+        this.position.add(speedX / SIDE, speedY / SIDE);
     }
 
-    public boolean collideHitbox(Hitbox hitbox){
+    boolean collideHitbox(Hitbox hitbox){
         return this.hitbox.collide(hitbox);
     }
+
     private void knockBack(){
         this.speedX = - this.speedX * 5 ;
         this.speedY = - this.speedY * 5 ;
     }
 
-    public void isAttacked(){
-        System.out.println("\t\t\tCOUCOU\t\t\t");
-        this.healthPoint -= 200;
-         knockBack();
+    void isAttacked(){
+        this.healthPoint -= 10;
+        knockBack();
     }
 
+    void setMainCharactersPosition(Coordinate position){
+        this.mainCharactersPosition = position;
+    }
 }
